f34fa21207f28fde21b67157b48a46b883e49034
jakobe <jakobe@debian.jakobe-PC>
2026-03-15 16:25:00 -0300

    Adiconando arquivos ao repositorio

---
+++ server.js
@@ -0,0 +1,14 @@
+const e = require("express");
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.use(express.static("public"))
+
+app.get("/post", (req, res) => {
+    res.sendFile(__dirname + "/public/post.html");
+});
+
+app.listen(port, () => {
+    console.log("blog tá rodando a todo vapor")
+});
